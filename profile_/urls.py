4c800d0304d9306e4692e1ecc902c8b2f5142925
YmnenkauaPy <ymnenkaua@gmail.com>
2024-11-09 15:22:52 +0200

    follow/unfollow

--- profile_/urls.py
+++ profile_/urls.py
@@ -3,6 +3,9 @@ from profile_ import views
 
 urlpatterns = [
     path('/profile/<int:user_id>', views.view_profile, name='profile'),
-    path('/update_profile/', views.update_profile, name='update_profile'),
-    path('/delete_profile/<int:user_id>', views.delete_profile, name='delete_profile'),
+    path('/update/profile/', views.update_profile, name='update_profile'),
+    path('/delete/profile/<int:user_id>', views.delete_profile, name='delete_profile'),
+    path('/search/for/users/', views.search_for_users, name='search_for_users'),
+    path('/related/users/<int:user_id>/<str:relation_type>/', views.related_users, name='related_users'),
+    path('/toggle/follow/user/<int:user_id>/', views.toggle_follow_user, name='follow_user'),
 ]
--- profile_/views.py
+++ profile_/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from authentication.models import CustomUser
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from profile_ import forms
+import os
 
 def view_profile(request, user_id):
     user = CustomUser.objects.get(id=user_id)
@@ -24,7 +27,40 @@ def delete_profile(request, user_id):
     user = CustomUser.objects.get(id=user_id)
 
     if request.method == 'POST':
+        if user.profile_picture:
+            image_path = user.profile_picture.path
+            if os.path.isfile(image_path):
+                os.remove(image_path)
+
         user.delete() 
         return redirect('main') 
 
     return render(request, 'profile/delete_profile.html')
+
+def search_for_users(request):
+    search = request.GET.get('search', '') 
+    users = CustomUser.objects.filter(username__icontains=search) if search else []
+    return render(request, 'profile/search_results.html', {'users': users, 'type':'results of searching'})
+
+def related_users(request, user_id, relation_type):
+    user = CustomUser.objects.get(id=user_id)
+    related_users = getattr(user, relation_type).all()
+    return render(request, 'profile/search_results.html', {'users': related_users, 'type':relation_type})
+
+def toggle_follow_user(request, user_id):
+    if request.method == 'POST':
+        current_user = request.user
+        user_to_toggle = get_object_or_404(CustomUser, id=user_id)
+        
+        if user_to_toggle != current_user:
+            if user_to_toggle in current_user.followings.all():
+                current_user.followings.remove(user_to_toggle)  # Отписываемся
+                status = 'unfollowed'
+            else:
+                current_user.followings.add(user_to_toggle)  # Подписываемся
+                status = 'followed'
+            current_user.save()
+            
+            return JsonResponse({'status': 'ok', 'follow_status': status})
+    
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
